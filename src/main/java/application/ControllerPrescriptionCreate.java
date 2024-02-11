29d834cbd8fe8cff90ab0fcc337463609ba0093c
LeSci-0x1 <sscire@csumb.edu>
2024-02-10 19:39:36 -0800

    2024.02.10: implemented createPatient, showPatient, updatePatient, createPrescription

--- src/main/java/application/ControllerPrescriptionCreate.java
+++ src/main/java/application/ControllerPrescriptionCreate.java
@@ -1,7 +1,6 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.Random;
 
@@ -39,26 +38,110 @@ public class ControllerPrescriptionCreate {
 		 * valid doctor name and id
 		 */
 		//TODO
+		int doctor_id = 0;
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select id " +
+							"from doctor " +
+							"where id=? and last_name=? and first_name=?");
+			ps.setInt(1, p.getDoctor_id());
+			ps.setString(2, p.getDoctorLastName());
+			ps.setString(3, p.getDoctorFirstName());
 
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				doctor_id = rs.getInt(1);
+			} else {
+				model.addAttribute("message", "Doctor not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_create";
+		}
 		/*
 		 * valid patient name and id
 		 */
 		//TODO
+		int patient_id = 0;
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select id " +
+							"from patient " +
+							"where id=? and last_name=? and first_name=?");
+			ps.setInt(1, p.getPatient_id());
+			ps.setString(2, p.getPatientLastName());
+			ps.setString(3, p.getPatientFirstName());
 
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				patient_id = rs.getInt(1);
+			} else {
+				model.addAttribute("message", "Patient not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_create";
+		}
 		/*
 		 * valid drug name
 		 */
 		//TODO
+		int drug_id = 0;
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select id " +
+							"from drug " +
+							"where name=?");
+			ps.setString(1, p.getDrugName());
 
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				drug_id = rs.getInt(1);
+			} else {
+				model.addAttribute("message", "Drug not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_create";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_create";
+		}
 		/*
 		 * insert prescription  
 		 */
 		//TODO 
-		
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"insert into prescription(drug_id, doctor_id, patient_id, quantity, refills, create_date ) " +
+							"values(?, ?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, drug_id);
+			ps.setInt(2, doctor_id);
+			ps.setInt(3, patient_id);
+			ps.setInt(4, p.getQuantity());
+			ps.setInt(5, p.getRefills());
+			ps.setString(6, LocalDate.now().toString());
 
-		model.addAttribute("message", "Prescription created.");
-		model.addAttribute("prescription", p);
-		return "prescription_show";
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) p.setRxid(rs.getInt(1));
+
+			model.addAttribute("message", "Prescription created.");
+			model.addAttribute("prescription", p);
+			return "prescription_show";
+
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_create";
+		}
 	}
 	
 	private Connection getConnection() throws SQLException {
