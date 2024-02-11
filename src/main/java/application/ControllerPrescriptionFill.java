9aa04cb1f413e77899bea5d4744f88d4b5d6139f
LeSci-0x1 <sscire@csumb.edu>
2024-02-10 22:24:42 -0800

    2024.02.10: implemented createPatient, showPatient, updatePatient, createPrescription, implementing prescriptionFill

--- src/main/java/application/ControllerPrescriptionFill.java
+++ src/main/java/application/ControllerPrescriptionFill.java
@@ -1,7 +1,8 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -34,31 +35,213 @@ public class ControllerPrescriptionFill {
 		/*
 		 * valid pharmacy name and address, get pharmacy id and phone
 		 */
-		// TODO 
-		
-		// TODO find the patient information 
+		// TODO
+
+		int pharmacy_id = 0;
+		String phone = "";
+
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select id, phone " +
+							"from pharmacy " +
+							"where name=? and address=?");
+			ps.setString(1, p.getPharmacyName());
+			ps.setString(2, p.getPharmacyAddress());
+
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				pharmacy_id = rs.getInt(1);
+				phone = rs.getString(2);
+			} else {
+				model.addAttribute("message", "Pharmacy not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
+		// TODO find the patient information
+		int patient_id = 0;
+		String p_last_name = "";
+		String p_first_name = "";
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select id, last_name, first_name " +
+							"from patient " +
+							"where last_name=?");
+			ps.setString(1, p.getPatientLastName());
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				patient_id = rs.getInt(1);
+				p_last_name = rs.getString(2);
+				p_first_name = rs.getString(3);
+			} else {
+				model.addAttribute("message", "Patient not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
 
 		// TODO find the prescription 
+		int rxid_id = 0;
+		int doctor_id = 0;
+		int drug_id = 0;
+		int refills = 0;
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select rxid, doctor_id, drug_id, refills" +
+							"from prescription " +
+							"where rxid=?");
+			ps.setInt(1, p.getRxid());
 
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				rxid_id = rs.getInt(1);
+				doctor_id = rs.getInt(2);
+				drug_id = rs.getInt(3);
+				refills = rs.getInt(4);
+			} else {
+				model.addAttribute("message", "Prescription not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
 
 		/*
 		 * have we exceeded the number of allowed refills
 		 * the first fill is not considered a refill.
 		 */
 		
-		// TODO 
+		// TODO
+		int count = 0;
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select count(rxid)" +
+							"from prescription_fill " +
+							"where rxid=?");
+			ps.setInt(1, p.getRxid());
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				count = rs.getInt(1);
+			}
+			int remaining = refills - count;
+			if (remaining < 1 ){
+				model.addAttribute("message", "No prescription refills remaining.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			} else {
+				model.addAttribute("message", "Prescription refills remaining: " + remaining);
+				model.addAttribute("prescription", p);
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
 		
 		/*
 		 * get doctor information 
 		 */
 		// TODO
+		String dr_last_name = "";
+		String dr_first_name = "";
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select last_name, first_name " +
+							"from doctor " +
+							"where id=?");
+			ps.setInt(1, doctor_id);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				dr_last_name = rs.getString(2);
+				dr_first_name = rs.getString(3);
+			} else {
+				model.addAttribute("message", "Doctor not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
 
 		/*
 		 * calculate cost of prescription
 		 */
 		// TODO 
-		
-		// TODO save updated prescription 
+		int qtyPharmacy = p.getQuantity();
+		BigDecimal cost = new BigDecimal("0.0");
+		BigDecimal price = new BigDecimal("0.0");
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"select quantity, price " +
+							"from drug_cost " +
+							"where drug_id=? and pharmacy_id=?");
+			ps.setInt(1, drug_id);
+			ps.setInt(1, pharmacy_id);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				qtyPharmacy = rs.getInt(1);
+				price = rs.getBigDecimal(2);
+				cost = price.multiply(BigDecimal.valueOf(p.getQuantity()));
+			} else {
+				model.addAttribute("message", "Drug not found.");
+				model.addAttribute("prescription", p);
+				return "prescription_fill";
+			}
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
+		// TODO save updated prescription
+		// insert record to prescription_fill
+		try (Connection con = getConnection();) {
+			PreparedStatement ps = con.prepareStatement(
+					"insert into prescription_fill(rxid_id, pharmacy_id, cost, fill_date) " +
+							"values(?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, rxid_id);
+			ps.setInt(2, pharmacy_id);
+			ps.setBigDecimal(3, cost);
+			ps.setString(4, LocalDate.now().toString());
+
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) p.setRxid(rs.getInt(1));
+		} catch (SQLException e) {
+			model.addAttribute("message", "SQL Error."+e.getMessage());
+			model.addAttribute("prescription", p);
+			return "prescription_fill";
+		}
+
+		// update prescription
+
+
 
 		// show the updated prescription with the most recent fill information
 		model.addAttribute("message", "Prescription filled.");
